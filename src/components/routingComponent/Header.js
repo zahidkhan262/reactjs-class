7dea52058d2d97136074cd735256add5d7e0c420
zahidkhan262 <zahidkhan007860@gmail.com>
2022-01-17 22:35:21 +0530

    add components

--- src/components/routingComponent/Header.js
+++ src/components/routingComponent/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
         <>
             <NavLink exact to="/" >Home</NavLink>
             <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact us</NavLink>
             <NavLink to="/service">Services</NavLink>
+            <NavLink to="/contact">Contact us</NavLink>
         </>
     )
 }
